3eaf64b55fd16533611b8ba42498acf604db9f2c
Iam2smart <calebtrobins@gmail.com>
2017-08-26 14:06:57 -0600

    added devicesupport

---
+++ Yalu1031/devicesupport.h
@@ -0,0 +1,35 @@
+#ifndef devicesupport_h
+#define devicesupport_h
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+unit64_t constget(int idx);
+#import <stdint.h>
+struct mac_policy_ops{
+	unit64_t mpo_audit_check

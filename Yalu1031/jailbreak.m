5d2d08b3ede95de1ca698b23b18bc01a77fc9489
Iam2smart <calebtrobins@gmail.com>
2017-08-26 12:05:47 -0600

    added jailbreak.m

---
+++ Yalu1031/jailbreak.m
@@ -0,0 +1,16 @@
+//
+// jailbreak.m
+// yalu1031
+//
+// Created by the jailbreak community
+
+#import <Foundation/Foudation.h>
+#undef __IPHONE_OS_VERSION_MIN_REQUIRED
+#import <mach/mach.h>
+#import "devicesupport.h"
+
+
+#import <IOKIT/IOKitLib.h>
+#import <dlfcn.h>
+#import <Foundation/Foudation.h>
+#import <UIKit.UIKit.h>

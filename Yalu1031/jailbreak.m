20b2d00ced44bdcb8c61097c975d03d4feffa38a
Iam2smart <calebtrobins@gmail.com>
2017-08-26 13:01:09 -0600

    Update jailbreak.m

--- Yalu1031/jailbreak.m
+++ Yalu1031/jailbreak.m
@@ -3,6 +3,7 @@
 // yalu1031
 //
 // Created by the jailbreak community
+//
 
 #import <Foundation/Foudation.h>
 #undef __IPHONE_OS_VERSION_MIN_REQUIRED
@@ -14,3 +15,7 @@
 #import <dlfcn.h>
 #import <Foundation/Foudation.h>
 #import <UIKit.UIKit.h>
+#import <pthread.h>
+#import <mach/mach.h>
+
+#import "devicesupport.h"
